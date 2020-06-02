49b521e8e634618ba9b7e7844f2ea7cdc81f5921
justinhtn <justinhoughton@fastmail.com>
2020-06-02 17:45:32 +0000

    fixes linting errors

--- pixl/utils.py
+++ pixl/utils.py
@@ -1,19 +1,21 @@
 from tensorflow import keras
 from tensorflow.keras.applications import xception
 from tensorflow.keras.preprocessing import image
-from tensorflow.keras.models import Model
 from tensorflow.keras.backend import expand_dims
 
-model_target_size = (299,299)
+model_target_size = (299, 299)
 
 class Pixl(object):
     
+
     def __init__(self):
-        
+
         model = xception.Xception(weights='imagenet', pooling='avg')
         layer_name = 'avg_pool'
-        self.intermediate_layer_model = keras.Model(inputs=model.input,
-                                               outputs=model.get_layer(layer_name).output)
+        self.intermediate_layer_model = keras.Model(
+            inputs=model.input,
+            outputs=model.get_layer(layer_name).output
+            )
 
     def get_vec(self, img_path):
         """
@@ -21,16 +23,16 @@ class Pixl(object):
 
         Args:
         img_path (str): string specifying location of image.
-        
+
         Returns:
         layer_output (numpy array): numpy array of intermediate layer.
         """
 
         # loads image and returns a pil object
-        img = image.load_img(img_path, target_size= model_target_size)
+        img = image.load_img(img_path, target_size=model_target_size)
         # uses pil object and returns a 3d numpy array
         x = image.img_to_array(img)
-        # adds additional dimension. Model expects 4 and we currently have 3. 
+        # adds additional dimension. Model expects 4 and we currently have 3.
         x = expand_dims(x, axis=0)
         # pre processing numpy array scaled between -1 and 1
         x = xception.preprocess_input(x)
--- setup.py
+++ setup.py
@@ -3,13 +3,15 @@ from setuptools import find_packages
 
 setup(name='pixl',
       version='0.1.6.2',
-      description='A simple package that takes in an image and returns a vector.',
+      description='A simple package that takes in\
+      an image and returns a vector.',
       url='https://github.com/justinhtn/pixl',
       author='Justin Houghton',
       author_email='justin.houghton@gmail.com',
       license='MIT',
-      packages=['pixl'],
+      packages=find_packages(),
       include_package_data=True,
       zip_safe=False,
       python_requires='>=3.6'
       )
+      
